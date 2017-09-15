1d7467d5420acf8b0178ccac4f60059e3350a392
mromero <michael.romero09@gmail.com>
2017-09-14 19:40:24 -0700

    Tried to sync with remote master branch
    had to change some of Becca's stupid crap

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,9 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 // import '../App.css';
 import { Route } from 'react-router-dom';
 import {WelcomePage} from './welcomePage.js';
 import {AdminMain} from './adminMain.js';
 import {ShopMain} from './shopMain.js';
-=======
-import '../App.css';
-import { Route } from 'react-router-dom';
-import { WelcomePage } from './welcomePage.js';
-// import {AdminMain} from './adminMain.js';
-import { ShopMain } from './shopMain.js';
->>>>>>> 5a9f3bc2fc347c2dcc9659c0e4fc5a6055db3f97
 
 class App extends Component {
   render() {
