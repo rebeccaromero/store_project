6487fc4f3d9bab4601dfa59871800137e1ae770d
mromero <michael.romero09@gmail.com>
2017-09-14 18:59:41 -0700

    added CSRF Token & edited login component

--- src/components/adminLogin.js
+++ src/components/adminLogin.js
@@ -1,13 +1,36 @@
 import React from 'react';
+import axios from 'axios';
+import axiosDefaults from 'axios/lib/defaults';
 
 import { Login } from '../containers/login';
 
+axiosDefaults.xsrfCookieName = "csrftoken"
+axiosDefaults.xsrfHeaderName = "X-CSRFToken"
+
 export class AdminLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.submitAdminData = this.submitAdminData.bind(this);
+  }
+
+  submitAdminData(email, password) {
+    console.log('ADMIN LOGIN: ' + email + ', ' + password);
+    axios.request({
+      method: 'post',
+      url: 'http://localhost:8000/admin/login',
+      data: {
+        email: email,
+        password: password
+      },
+      headers: { "X-CSRFToken": 'csrfToken' }
+    });
+  }
+
   render() {
     return (
       <div>
         <h1>Admin Login</h1>
-        <Login />
+        <Login type={"admin"} submitAdminData={this.submitAdminData} />
       </div>
     )
   }
--- src/containers/login.js
+++ src/containers/login.js
@@ -5,17 +5,49 @@ import {
 } from 'react-router-dom';
 
 export class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            email: "",
+            password: ""
+        };
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        console.log('Form Type: ' + e.target.type.value);
+        var email = e.target.email.value;
+        var password = e.target.password.value;
+        this.setState({
+            email: "",
+            password: ""
+        });
+        this.props.submitAdminData(email, password);
+    }
+
+    handleEmail(e) {
+        this.setState({ email: e.target.value });
+    }
+    
+    handlePassword(e) {
+        this.setState({ password: e.target.value });
+    }
+
     render() {
         return (
             <div>
-                <form className="form-inline" action="{% url 'login_submit' %}" method='post'>
+                <form className="form-inline" onSubmit={this.handleSubmit}>
+                    <input type="hidden" name="type" value={this.props.type} />
                     <div className="form-group">
                         <label htmlFor="">Email:</label>
-                        <input className="form-control" type="text" name='email' />
+                        <input className="form-control" type="text" name='email' value={this.state.email} onChange={this.handleEmail}/>
                     </div>
                     <div className="form-group">
                         <label htmlFor="">Password:</label>
-                        <input className="form-control" type="password" name='password' />
+                        <input className="form-control" type="password" name='password' value={this.state.password} onChange={this.handlePassword}/>
                     </div>
                     <div className="login-submit">
                         <input type="submit" value='Login' />
